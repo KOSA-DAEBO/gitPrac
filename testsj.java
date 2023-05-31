e3a472dd0bee672e65f3da4e96b1df764c8b875e
Jaejin Lee <jaejin1112@gmail.com>
2023-05-31 17:46:19 +0900

    자료 다시 넣어두기

---
+++ testsj.java
@@ -0,0 +1,7 @@
+public class testsj {
+    public static void main(String[] args) {
+        System.out.println("hello");
+	  BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	  StringTokenizer st;
+    }
+}
